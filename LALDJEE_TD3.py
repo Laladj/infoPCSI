ed57366dad75e9ff09c6f0abfc4c8a47795d7a0e
John Doe <johndoe@example.com>
2024-11-23 12:09:45 +0100

    DM3 +TD3

--- LALDJEE_TD3.py
+++ LALDJEE_TD3.py
@@ -10,6 +10,22 @@
 from matplotlib import pyplot as plt
 ###EXERCICE 1 : recherche dichotomique dans un tableau trié
 
-"""
+def recherche_dico(L:list, x:int)->int:
+    """ Argument
+        L :liste d'entiers rangés dans l'ordre
+        x : entier comparable à L
+        renvoie la position """
+    f, g = L[0], L[-1]
+    mod = 0
+    while f-g > 1:
+        mod=(f+g)//2
+        if L[mod]==x:
+            return mod
+        elif L[mod]<x:
+            g = g//2
+        elif L[mod]>x:
+            f + g//2
+    return mod
 
-"""
+test = [x for x in range(2024)]
+recherche_dico(test,30)
